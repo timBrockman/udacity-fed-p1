f956b4e45ad057bc807f5aff6b3b16d968efeacb
timBrockman <tim@timbrockman.com>
2015-10-06 14:43:59 -0400

    add node dev dependancies and gruntfile.js

---
+++ gruntfile.js
@@ -0,0 +1,43 @@
+/*
+some simple tasks
+for a simple page
+*/
+module.exports = function(grunt){
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		concat: {
+			dist:{
+				src:[
+					'src/js/sanity1.js',
+					'src/js/sanity2.js'
+				],
+				dest:'dist/js/isSane.js'
+			}
+		},
+		responsive_images: {
+			dev:{
+				files:[{
+					expand: true,
+					src: ['app/img/**/*.{jpg,gif,png}'],
+					cwd:'src/',
+					dest:'dist/'
+				}]
+			}
+		},
+		copy:{
+			dev:{
+				files:[{
+					expand:true,
+					src:['**/*', '!app/img/**/*.*'],
+					cwd: 'src/',
+					dest: 'dist/'
+				}]
+			}
+		}
+	});
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-responsive-images');
+	grunt.loadNpmTasks('grunt-contrib-imagemin');
+	grunt.registerTask('default', ['copy'])//,'responsive_images','concat']);
+}
